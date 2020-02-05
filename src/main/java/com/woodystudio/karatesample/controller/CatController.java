5496775f3e81abe6337726c82239e9bff6178b99
Songbai Yan <colorguitar@hotmail.com>
2020-02-05 12:55:50 +0800

    added cat controller with one get method

--- src/main/java/com/woodystudio/karatesample/controller/CatController.java
+++ src/main/java/com/woodystudio/karatesample/controller/CatController.java
@@ -1,4 +1,14 @@
 package com.woodystudio.karatesample.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cat")
 public class CatController {
+    @GetMapping()
+    public String getCat() {
+        return "one cat";
+    }
 }
