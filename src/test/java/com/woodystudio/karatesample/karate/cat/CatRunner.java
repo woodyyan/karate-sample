53048819782fe15c567f243992b942c8aef9696a
Songbai Yan <colorguitar@hotmail.com>
2020-12-10 09:39:35 +0800

    backup something

--- src/test/java/com/woodystudio/karatesample/karate/cat/CatRunner.java
+++ src/test/java/com/woodystudio/karatesample/karate/cat/CatRunner.java
@@ -4,7 +4,7 @@ import com.intuit.karate.KarateOptions;
 import com.intuit.karate.junit4.Karate;
 import org.junit.runner.RunWith;
 
-@KarateOptions(features = "classpath:com/woodystudio/karatesample/karate/cat/cats.feature")
+@KarateOptions(features = "classpath:com1/woodystudio/karatesample/karate/cat/cats.feature")
 @RunWith(Karate.class)
 public class CatRunner {
 }
--- src/test/java/com/woodystudio/karatesample/karate/cat/cats.feature
+++ src/test/java/com/woodystudio/karatesample/karate/cat/cats.feature
@@ -1,12 +1,12 @@
 Feature: get cat test
 
-#Background:
-#* url 'localhost:8080'
+Background:
+* url 'http://localhost:8080'
 
 Scenario: retrieve a cat
 
 # get cat
-Given url 'http://localhost:8080/cat'
+Given path 'cat'
 When method get
 Then status 200
 And match response == 'one cat'
